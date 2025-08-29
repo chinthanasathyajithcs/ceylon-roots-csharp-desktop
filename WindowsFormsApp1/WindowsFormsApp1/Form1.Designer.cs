86a3701cf25029c4aa0575cffbc80beb80bef53a
isii-30 <manjulakalubowila074@gmail.com>
2025-08-29 13:22:03 +0530

    continue button black

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -40,8 +40,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1000, 266);
+            this.panel1.Size = new System.Drawing.Size(1500, 416);
             this.panel1.TabIndex = 0;
             // 
             // label1
@@ -49,23 +50,25 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Navy;
-            this.label1.Location = new System.Drawing.Point(288, 87);
+            this.label1.Location = new System.Drawing.Point(432, 136);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(415, 97);
+            this.label1.Size = new System.Drawing.Size(662, 156);
             this.label1.TabIndex = 0;
             this.label1.Text = "WELCOME";
             // 
             // Countinue_button
             // 
-            this.Countinue_button.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.Countinue_button.BackColor = System.Drawing.Color.Black;
             this.Countinue_button.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Countinue_button.FlatAppearance.BorderSize = 0;
             this.Countinue_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Countinue_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Countinue_button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(252)))), ((int)(((byte)(202)))));
-            this.Countinue_button.Location = new System.Drawing.Point(408, 350);
+            this.Countinue_button.Location = new System.Drawing.Point(612, 547);
+            this.Countinue_button.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Countinue_button.Name = "Countinue_button";
-            this.Countinue_button.Size = new System.Drawing.Size(173, 56);
+            this.Countinue_button.Size = new System.Drawing.Size(260, 88);
             this.Countinue_button.TabIndex = 1;
             this.Countinue_button.Text = "Continue";
             this.Countinue_button.UseVisualStyleBackColor = false;
@@ -73,11 +76,12 @@
             // 
             // Welcome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.ClientSize = new System.Drawing.Size(1500, 781);
             this.Controls.Add(this.Countinue_button);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Welcome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ceylon Roots";
