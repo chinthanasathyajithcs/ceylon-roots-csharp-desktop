8cb820938ffa40078ca6bf9c0a23f7bf68c2d48b
ChinthanaSathyajith <chinthanasathyajith5@gmail.com>
2025-08-29 14:45:02 +0530

    Update Form1.Designer.cs

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -3,6 +3,7 @@
     partial class Welcome
     {
         /// <summary>
+        /// test
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
