2610a37ed20c26959994faf2bbecc2fd25f2bfea
samuditha sarasasi <samudhitha2003@outlook.com>
2025-09-01 12:51:20 +0530

    add button

--- WindowsFormsApp1/WindowsFormsApp1/customer.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/customer.Designer.cs
@@ -51,6 +51,9 @@
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -111,7 +114,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(784, 144);
+            this.label5.Location = new System.Drawing.Point(780, 144);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(148, 24);
             this.label5.TabIndex = 5;
@@ -152,11 +155,12 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(659, 219);
+            this.label6.Location = new System.Drawing.Point(659, 217);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(224, 28);
             this.label6.TabIndex = 10;
             this.label6.Text = "Delivery Information";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
@@ -192,7 +196,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(785, 292);
+            this.label10.Location = new System.Drawing.Point(780, 292);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(140, 24);
             this.label10.TabIndex = 14;
@@ -202,7 +206,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(785, 338);
+            this.label11.Location = new System.Drawing.Point(780, 342);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(42, 24);
             this.label11.TabIndex = 15;
@@ -212,7 +216,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(785, 391);
+            this.label12.Location = new System.Drawing.Point(780, 391);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(226, 24);
             this.label12.TabIndex = 16;
@@ -260,12 +264,48 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(172, 22);
             this.dateTimePicker1.TabIndex = 22;
             // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(914, 442);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 35);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(1053, 442);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 36);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Submit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Bahnschrift", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(397, 446);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(285, 25);
+            this.checkBox1.TabIndex = 25;
+            this.checkBox1.Text = "Leave Package at hotel reception";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(217)))), ((int)(((byte)(196)))));
             this.ClientSize = new System.Drawing.Size(1248, 494);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox8);
@@ -323,5 +363,8 @@
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
--- WindowsFormsApp1/WindowsFormsApp1/customer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/customer.cs
@@ -31,5 +31,20 @@ namespace WindowsFormsApp1
         {
 
         }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
