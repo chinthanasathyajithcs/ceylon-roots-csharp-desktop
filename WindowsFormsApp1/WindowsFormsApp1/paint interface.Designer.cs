38e18a85ba1e669243ef16e444131876a60ef198
VPJayasinghe <vageeshaprasadini@gmail.com>
2025-09-02 22:34:06 +0530

    paint interface code updated

--- WindowsFormsApp1/WindowsFormsApp1/paint interface.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/paint interface.Designer.cs
@@ -67,6 +67,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Open Paint";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
@@ -158,6 +159,7 @@
             this.button3.TabIndex = 9;
             this.button3.Text = "Save";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // paint_interface
             // 
