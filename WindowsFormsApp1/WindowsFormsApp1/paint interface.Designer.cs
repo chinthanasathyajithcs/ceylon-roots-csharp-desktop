ac9439b573351ecfb037eeb9055f6c14c6e99cbc
NimeshaWijegunawardana <nimeshawijegunawardana2005@gmail.com>
2025-09-08 19:20:30 +0530

    paint

--- WindowsFormsApp1/WindowsFormsApp1/paint interface.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/paint interface.Designer.cs
@@ -38,6 +38,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -46,9 +48,9 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(217)))), ((int)(((byte)(196)))));
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(1254, 320);
+            this.button1.Location = new System.Drawing.Point(675, 0);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(297, 54);
+            this.button1.Size = new System.Drawing.Size(1111, 143);
             this.button1.TabIndex = 0;
             this.button1.Text = "Open photo";
             this.button1.UseVisualStyleBackColor = false;
@@ -58,14 +60,13 @@
             // 
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(217)))), ((int)(((byte)(196)))));
             this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Location = new System.Drawing.Point(0, 0);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(1644, 78);
+            this.button2.Size = new System.Drawing.Size(680, 143);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Open Paint";
+            this.button2.Text = "      Open Paint";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -74,16 +75,16 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(12, 117);
+            this.label1.Location = new System.Drawing.Point(984, 563);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(436, 39);
+            this.label1.Size = new System.Drawing.Size(419, 38);
             this.label1.TabIndex = 2;
             this.label1.Text = "Turn memories into designs";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.QR;
-            this.pictureBox1.Location = new System.Drawing.Point(76, 173);
+            this.pictureBox1.Location = new System.Drawing.Point(1039, 245);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(303, 286);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -95,7 +96,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(90, 482);
+            this.label2.Location = new System.Drawing.Point(1046, 624);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(271, 29);
             this.label2.TabIndex = 4;
@@ -106,7 +107,7 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(27, 541);
+            this.label3.Location = new System.Drawing.Point(38, 412);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(399, 29);
             this.label3.TabIndex = 5;
@@ -117,7 +118,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(12, 597);
+            this.label4.Location = new System.Drawing.Point(38, 502);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(390, 29);
             this.label4.TabIndex = 6;
@@ -129,11 +130,11 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(12, 626);
+            this.label5.Location = new System.Drawing.Point(38, 551);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(493, 29);
+            this.label5.Size = new System.Drawing.Size(329, 29);
             this.label5.TabIndex = 7;
-            this.label5.Text = "Please click the \"Save and Next Page\" button";
+            this.label5.Text = "Please click the \"Save\" button";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
@@ -141,7 +142,7 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(12, 655);
+            this.label6.Location = new System.Drawing.Point(32, 602);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(601, 29);
             this.label6.TabIndex = 8;
@@ -151,22 +152,45 @@
             // 
             this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(217)))), ((int)(((byte)(196)))));
             this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(0, 737);
+            this.button3.Location = new System.Drawing.Point(0, 711);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(1644, 66);
+            this.button3.Size = new System.Drawing.Size(680, 94);
             this.button3.TabIndex = 9;
             this.button3.Text = "Save";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(29, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(604, 29);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Click  \"Open Paint\" to create a new design from scratch.";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label8.Location = new System.Drawing.Point(867, 175);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(660, 29);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Click \"Open photo\" to edit or design on your uploaded photo.";
             // 
             // paint_interface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(52)))), ((int)(((byte)(46)))));
-            this.ClientSize = new System.Drawing.Size(1644, 803);
+            this.ClientSize = new System.Drawing.Size(1784, 803);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -177,8 +201,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.MaximumSize = new System.Drawing.Size(1662, 850);
-            this.MinimumSize = new System.Drawing.Size(1662, 850);
+            this.MaximumSize = new System.Drawing.Size(1802, 850);
+            this.MinimumSize = new System.Drawing.Size(1772, 850);
             this.Name = "paint_interface";
             this.Text = "paint_interface";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -200,5 +224,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
