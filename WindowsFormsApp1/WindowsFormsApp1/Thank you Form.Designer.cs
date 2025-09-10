9a3192ec43b341a061b1695dde4689265a28c3b5
VPJayasinghe <vageeshaprasadini@gmail.com>
2025-09-10 16:41:13 +0530

    gg

--- WindowsFormsApp1/WindowsFormsApp1/Thank you Form.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Thank you Form.Designer.cs
@@ -28,18 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(0, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
             // 
             // label2
             // 
@@ -56,7 +46,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Thank_you_Form";
             this.Text = "Thank_you_Form";
             this.ResumeLayout(false);
@@ -65,8 +54,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
 }
