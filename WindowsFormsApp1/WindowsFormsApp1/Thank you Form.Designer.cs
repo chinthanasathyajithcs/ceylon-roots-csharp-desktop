265994da30c35aeb32523a5315ff70e0d91e8deb
NimeshaWijegunawardana <nimeshawijegunawardana2005@gmail.com>
2025-09-11 10:33:28 +0530

    a

--- WindowsFormsApp1/WindowsFormsApp1/Thank you Form.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Thank you Form.Designer.cs
@@ -28,48 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Thank_you_Form));
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            resources.ApplyResources(this.label2, "label2");
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(640, 357);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(617, 135);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Thank you\r\n";
             // 
             // button1
             // 
+            resources.ApplyResources(this.button1, "button1");
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(217)))), ((int)(((byte)(196)))));
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(797, 718);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(320, 59);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Thank_you_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(52)))), ((int)(((byte)(46)))));
-            this.ClientSize = new System.Drawing.Size(1900, 953);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
-            this.MaximumSize = new System.Drawing.Size(1918, 1000);
-            this.MinimumSize = new System.Drawing.Size(1918, 1000);
             this.Name = "Thank_you_Form";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Thank_you_Form";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
